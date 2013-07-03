99b69ceca495988282db1e376fff647e6476385d
Fuad Ahmed <fahmed@metrouni.edu.bd>
2013-07-03 23:11:43 +0500

    -> Six models renamed.[ExamMarks, ExamRegistration, MarkingSchame, ModuleRegistration, OfferedModule, TermAdmission]

--- protected/components/DBhelper.php
+++ protected/components/DBhelper.php
@@ -81,7 +81,36 @@ class DBhelper {
         return  $model->per_title." ".$model->per_firstName." ".$model->per_lastName;
     }
         
+    public static function getStudentId($sectionName,$batchName,$programmeCode)
+    {
+       $sql = "SELECT MAX(CONVERT(SUBSTRING_INDEX(a.studentID,'-',-1),UNSIGNED INTEGER)) AS num ,SUBSTRING(CONVERT(b.bat_year, char),-2) as acYear, a.sectionName, a.programmeCode , s.sec_startId ,s.sec_endId,b.bat_term FROM `tbl_admission` as a, `tbl_section` as s, `tbl_batch` as b WHERE a.sectionName=s.sectionName and a.batchName=s.batchName and a.programmeCode=s.programmeCode and"
+        . " s.batchName=b.batchName and s.programmeCode=b.programmeCode and a.sectionName='{$sectionName}' and a.batchName={$batchName} and a.programmeCode='{$programmeCode}' GROUP BY a.programmeCode limit 1;";    
+    
+       $list= Yii::app()->db->createCommand($sql)->query();
+ 
+       $rs= array();
+       
+        foreach($list as $item){
+    
+           $rs=$item;
+
+        }
         
+        $id=$rs['num']+1;
+        
+        if($id>=$rs['sec_startId'] && $id <=$rs['sec_endId'])
+        {
+            
+            if($id<10)$id="00".$id;
+            elseif($id<100)$id="0".$id;
+            
+            return $id=$rs['acYear'].$rs['bat_term']."-".$rs['programmeCode']."-".$id;
+        }
+        else 
+        {
+            return null;
+        }
+    }
         
         
         
